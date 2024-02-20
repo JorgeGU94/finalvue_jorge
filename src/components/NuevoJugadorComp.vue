1852cdd643d0f6fd029a7b1063d104b7d42ff8eb
JorgeGU <jordijgu@hotmail.com>
2024-02-20 23:38:18 +0100

    20/02/24
    mas cambios

--- src/components/NuevoJugadorComp.vue
+++ src/components/NuevoJugadorComp.vue
@@ -1,21 +1,20 @@
 <template>
-<form method="POST" @submit.prevent="insertarJugador(equipoSeleccionado)">
-    <label for="nombre">Nombre del Jugador</label><br>
-    <input type="text" name="nombre" v-model="nombre" required><br>
-    <label for="equipo">Equipo del Jugador</label><br>
-    <select name="equipo" v-model="equipoSeleccionado" v-if="equipoP == ''">
-        <option value="" disabled selected>Seleccione un equipo</option>
-        <option v-for="equipo in clubs" :key="equipo" :value="equipo.name">{{ equipo.name }}</option>
-    </select>
-    <select name="equipo" v-model="equipoSeleccionado" v-else>
-        <option :value="equipoP" selected>{{ equipoP }}</option>
-    </select>
-    <br>
-    <label for="goles">Goles del Jugador</label><br>
-    <input type="number" name="goles" v-model="goles" required><br>
-    <input type="submit" value="Añadir Jugador">
-</form>
-
+    <form method="POST" @submit.prevent="insertarJugador(equipoSeleccionado)">
+        <label for="nombre">Nombre del Jugador</label><br>
+        <input type="text" name="nombre" v-model="nombre" required><br>
+        <label for="equipo">Equipo del Jugador</label><br>
+        <select name="equipo" v-model="equipoSeleccionado" v-if="equipoP == ''">
+            <option value="" disabled selected>Seleccione un equipo</option>
+            <option v-for="equipo in clubs" :key="equipo" :value="equipo.name">{{ equipo.name }}</option>
+        </select>
+        <select name="equipo" v-model="equipoSeleccionado" v-else>
+            <option :value="equipoP" selected>{{ equipoP }}</option>
+        </select>
+        <br>
+        <label for="goles">Goles del Jugador</label><br>
+        <input type="number" name="goles" v-model="goles" required><br>
+        <input type="submit" value="Añadir Jugador">
+    </form>
 </template>
 
 <script>
--- src/views/JornadasVista.vue
+++ src/views/JornadasVista.vue
@@ -1,19 +1,17 @@
 <template>
     <div>
         <h1>Jornadas Liga</h1>
-        <input type="button" value="asd" @click="recogerJornadas()">
-        <table>
-            <tr>
-                <th>Jornada</th>
-            </tr>
-            <tr v-for="partido in matches" :key="partido">
-                <td>{{ partido.round }} / {{ partido.date }}</td>
-            </tr>
-        </table>
-        <label for="jornadas">Seleccione Jornada</label>
-        <select name="jornadas">
-            <option value=""></option>
+        <label for="jornadas">Seleccione Jornada</label><br>
+        <select name="jornadas" v-model="jornadaSeleccionada" @change="recogerPartidos(jornadaSeleccionada)">
+            <option :value="elemento" v-for="elemento in 16" :key="elemento">Jornada {{ elemento }}</option>
         </select>
+        <div v-if="this.matches.length > 0">
+            <div v-for="partido in matches" :key="partido">
+                <p>Fecha del Partido: {{ partido.date }}</p>
+                <p v-if="partido.score != null">{{ partido.team1 }} {{ partido.score[0] }} - {{ partido.score[1] }} {{ partido.team2 }}</p>
+                <p v-else>{{ partido.team1 }} <input type="text" size="2"> - <input type="text" size="2"> {{ partido.team2 }}</p>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -23,20 +21,40 @@ export default {
     data() {
         return {
             matches: {},
+            ultimaJornada: {},
+            numJornadas: "",
+            jornadaSeleccionada: ""
         }
     },
     methods: {
-        mostrarJornadas() {
+        recogerJornadas() {
+            let partidos = {};
+
             fetch("http://localhost:3000/matches")
             .then(response => response.json())
-            .then(json => this.matches = json)
-        },
-        recogerJornadas() {
+            .then(json => {
+                partidos = json;
+                let longitud = partidos.length;
 
+                fetch("http://localhost:3000/matches?id=" + longitud)
+                .then(response => response.json())
+                .then(json => {
+                    this.ultimaJornada = json;
+                    this.ultimaJornada.forEach(element => {
+                        this.numJornadas = element.round.substr(-2);
+                    });
+                })
+            })
+
+        },
+        recogerPartidos(param) {
+            console.log("http://localhost:3000/matches?round=Jornada " + param);
+            fetch("http://localhost:3000/matches?round=Jornada " + param)
+            .then(response => response.json())
+            .then(json => this.matches = json)
         }
     },
     created() {
-        this.mostrarJornadas();
         this.recogerJornadas();
     }
 }
